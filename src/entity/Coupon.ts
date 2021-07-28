dbeaaff2913d3a3043dfb4c973f8a85d27527234
fedapon <fedapon@gmail.com>
2021-07-28 19:43:13 -0300

    Coupon and Store entity were completed

--- src/entity/Coupon.ts
+++ src/entity/Coupon.ts
@@ -1,5 +1,31 @@
+import { Column, Entity } from "typeorm";
 import AbstractEntity from "./AbstractEntity";
 
+@Entity({name : 'coupons'})
 export default class Coupon extends AbstractEntity {
+
+    @Column({
+        length : 8
+    })
+    code : String
+
+    @Column({
+        name : 'expires_at',
+        type : 'time with time zone'
+    })
+    expiresAt : Date
+
+    @Column({
+        name : 'assigned_at',
+        type : 'time with time zone',
+        nullable : true
+    })
+    assignedAt : Date
+
+    @Column({
+        name : 'customer_email',
+        nullable : true
+    })
+    customerEmail : String
 
 }
--- src/entity/Store.ts
+++ src/entity/Store.ts
@@ -1,5 +1,16 @@
+import { Column, Entity } from "typeorm";
 import AbstractEntity from "./AbstractEntity";
 
+@Entity({name : 'stores'})
 export default class Store extends AbstractEntity {
-    
+
+    @Column({
+        length : 100
+    })
+    name : String
+
+    @Column({
+        length : 100
+    })
+    address : String
 }
