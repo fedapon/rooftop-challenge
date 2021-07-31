2fb899c3bd0b73c327806230bf4586b8d021fe34
fedapon <fedapon@gmail.com>
2021-07-31 17:34:10 -0300

    now code and expires_at are required to add a new code

--- src/validators/coupons/postValidation.ts
+++ src/validators/coupons/postValidation.ts
@@ -3,7 +3,7 @@ import joi from 'joi'
 const postValidation = joi.object({
     code : joi.string().alphanum().length(8).required(),
   
-    expires_at : joi.date().empty().greater('now')
+    expires_at : joi.date().empty().greater('now').required()
 
 })
 
