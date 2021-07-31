4760c9f564bba3f5dc2db48a3a8a5be6f51fa163
fedapon <fedapon@gmail.com>
2021-07-31 19:23:00 -0300

    added stores router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express = require('express')
 import dotenv = require('dotenv')
-import {couponsRouter} from './routes/coupons'
+import { couponsRouter } from './routes/coupons'
+import { storesRouter } from './routes/stores'
 
 dotenv.config()
 const app = express()
@@ -11,6 +12,9 @@ app.use( express.urlencoded({extended : false}))
 
 //endPoint => '/coupons'
 app.use(couponsRouter)
+
+//endPoint => '/stores'
+app.use(storesRouter)
 
 app.listen(process.env.EXPRESS_PORT, function() {
     console.log("Server started")
