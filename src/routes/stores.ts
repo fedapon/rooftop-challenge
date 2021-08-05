6e00d585bb5c43745ffe6a482628cde8c0627126
fedapon <fedapon@gmail.com>
2021-08-05 19:58:43 -0300

    moved logric from routes/stores to controlles/coupons

--- src/routes/stores.ts
+++ src/routes/stores.ts
@@ -4,51 +4,11 @@ import Store from '../entity/Store'
 import express = require('express')
 import postValidation from '../validators/stores/postValidation'
 import deleteValidation from '../validators/stores/deleteValidation'
+import { GetStoresAction } from '../controllers/stores/GetStoresAction'
 
 const storesRouter = express.Router()
 
-storesRouter.get('/stores', function(req, res) {
-    createConnection().then(async connection => {
-        //pagination calculation
-        let startItem = 0
-        let endItem = 10
-        if (req.query.hasOwnProperty('page')) {
-            if (Number(req.query.page)>0) {
-                startItem = Number(req.query.page) * 10
-                endItem = startItem + 10
-            }
-        }
-
-        //create query if name attribute is present
-        let query = {}
-        if (req.query.hasOwnProperty('name')) {
-            query['name'] = Like('%'+ req.query.name + '%')
-        }
-
-        //lets find stores
-        let repository = connection.getRepository(Store)
-        await repository.find(query)
-            .then(data => {
-                if (data == undefined) {
-                    res.status(200).json({messsaje : 'no data found'})
-                }
-                let msg = {
-                    count : data.length,
-                    stores : data.slice(startItem, endItem),
-                }
-                res.status(200).json(msg)
-            })
-            .catch(err=> {
-                res.status(404).json({message : err.message})
-            })
-
-        connection.close()
-        return
-    })
-    .catch(err=> {
-        return res.status(404).json({message : err.message})
-    })
-})
+storesRouter.get('/stores', GetStoresAction)
 
 storesRouter.post('/stores', function(req, res) {
     //validation
