5e269c518cb0e5e0a66524d280328441fa9893a3
Fernando Pons <fedapon@gmail.com>
2021-09-25 20:13:50 -0300

    refactoring stores to use one connection to DB

--- src/controllers/stores.ts
+++ src/controllers/stores.ts
@@ -1,131 +1,99 @@
 import 'reflect-metadata'
 import { createConnection, Like } from 'typeorm'
+import { connection } from '../app';
 import { Request, Response } from "express";
 import Store from '../entity/Store'
 import postValidation from '../validators/stores/postValidation';
 import deleteValidation from '../validators/stores/deleteValidation';
 
-export function GetStoresAction(req: Request, res: Response) {
-    createConnection()
-        .then(async connection => {
-            //pagination calculation
-            let startItem = 0
-            let endItem = 10
-            if (req.query.hasOwnProperty('page')) {
-                if (Number(req.query.page) > 0) {
-                    startItem = Number(req.query.page) * 10
-                    endItem = startItem + 10
-                }
+function errorResponse(res: Response, err: any, status: number) {
+    res.status(status).json({ message: err.message })
+    return
+}
+
+export async function GetStoresAction(req: Request, res: Response) {
+    let repository = (await connection).getRepository(Store)
+
+    //pagination calculation
+    let startItem = 0
+    let endItem = 10
+    if (req.query.hasOwnProperty('page')) {
+        if (Number(req.query.page) > 0) {
+            startItem = Number(req.query.page) * 10
+            endItem = startItem + 10
+        }
+    }
+
+    //create query if name attribute is present
+    let query = {}
+    if (req.query.hasOwnProperty('name')) {
+        query['name'] = Like('%' + req.query.name + '%')
+    }
+
+    //lets find stores
+    repository.find(query)
+        .then(data => {
+            if (data == undefined) {
+                res.status(200).json({ messsaje: 'no data found' })
+                return
             }
-
-            //create query if name attribute is present
-            let query = {}
-            if (req.query.hasOwnProperty('name')) {
-                query['name'] = Like('%' + req.query.name + '%')
+            let msg = {
+                count: data.length,
+                stores: data.slice(startItem, endItem),
             }
-
-            //lets find stores
-            let repository = connection.getRepository(Store)
-            repository.find(query)
-                .then(data => {
-                    if (data == undefined) {
-                        res.status(200).json({ messsaje: 'no data found' })
-                        connection.close()
-                        return
-                    }
-                    let msg = {
-                        count: data.length,
-                        stores: data.slice(startItem, endItem),
-                    }
-                    res.status(200).json(msg)
-                    connection.close()
-                    return
-                })
-                .catch(err => {
-                    res.status(404).json({ message: err.message })
-                    connection.close()
-                    return
-                })
-
+            res.status(200).json(msg)
+            return
         })
-        .catch(err => {
-            return res.status(404).json({ message: err.message })
-        })
+        .catch((err) => { errorResponse(res, err, 404) })
 }
 
 
-
-export function PostStoresAction(req: Request, res: Response): Response {
+export async function PostStoresAction(req: Request, res: Response) {
+    let repository = (await connection).getRepository(Store)
     //validation
     let validationResult = postValidation.validate(req.body)
     if (validationResult.error) {
         return res.status(422).json({ message: validationResult.error.details[0].message })
     }
 
-    createConnection()
-        .then(async connection => {
-            let repository = connection.getRepository(Store)
-
-            //lets add the new store
-            let newStore = {
-                name: req.body.name,
-                address: req.body.address
+    //lets add the new store
+    let newStore = {
+        name: req.body.name,
+        address: req.body.address
+    }
+    repository.save(newStore)
+        .then((data) => {
+            let msg = {
+                message: 'Store successfully created',
+                store: data
             }
-            repository.save(newStore)
-                .then((data) => {
-                    let msg = {
-                        message: 'Store successfully created',
-                        store: data
-                    }
-                    res.status(201).json(msg)
-                    connection.close()
-                    return
-                })
-                .catch((err) => {
-                    res.status(422).json({ message: err.message })
-                    connection.close()
-                    return
-                })
-
+            res.status(201).json(msg)
+            return
         })
-        .catch((err) => {
-            return res.status(422).json({ message: err.message })
-        })
+        .catch((err) => { errorResponse(res, err, 404) })
 }
 
 
-
-export function DeleteStoresAction(req: Request, res: Response): Response {
+export async function DeleteStoresAction(req: Request, res: Response) {
     //validation
     let validationResult = deleteValidation.validate(req.params)
     if (validationResult.error) {
         return res.status(422).json({ message: validationResult.error.details[0].message })
     }
 
-    createConnection()
-        .then(async connection => {
-            let repository = connection.getRepository(Store)
-            let storeToDelete = await repository.findOne(req.params)
+    let repository = (await connection).getRepository(Store)
+    let storeToDelete = await repository.findOne(req.params)
 
-            //check if store id exist
-            if (storeToDelete == undefined) {
-                res.status(422).json({ message: 'Store does not exist' })
-                connection.close()
-                return
-            }
+    //check if store id exist
+    if (storeToDelete == undefined) {
+        res.status(422).json({ message: 'Store does not exist' })
+        return
+    }
 
-            await repository.delete(storeToDelete)
-                .then(() => {
-                    res.status(201).json({ message: 'Store successfully deleted' })
-                })
-                .catch((err) => {
-                    res.status(422).json({ message: err.message })
-                })
-
-            connection.close()
+    await repository.delete(storeToDelete)
+        .then(() => {
+            res.status(201).json({ message: 'Store successfully deleted' })
             return
         })
-        .catch((err) => {
-            return res.status(422).json({ message: err.message })
-        })
+        .catch((err) => { errorResponse(res, err, 404) })
 }
